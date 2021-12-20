6b1feb93067f6ff54a4e1965c52e9012dcd96df5
Alexey <paradigmofc@gmail.com>
2021-12-20 20:34:08 +0200

    form fix

--- mail.php
+++ mail.php
@@ -15,13 +15,13 @@ $select = $_POST['user_selection'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'lexa.animewnik@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'slptripvwzycyieu'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'sport.nutritionist@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'BzPaSM0R0ucN0ht6v2FN'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('Admin');  // от кого будет уходить письмо?
-$mail->addAddress('lexa.animewnik@gmail.com');  // Кому будет уходить письмо 
+$mail->setFrom('sport.nutritionist@mail.ru');  // от кого будет уходить письмо?
+$mail->addAddress('sport.nutritionist@mail.ru');  // Кому будет уходить письмо 
 $mail->isHTML(true);                          
 
 $mail->Subject = 'Заявка с сайта Kotenko nutri';
