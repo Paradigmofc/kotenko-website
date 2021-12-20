3a524b2826f08950f311c4e918271b42d51cc64e
Alexey <paradigmofc@gmail.com>
2021-12-20 15:03:43 +0200

    animations

--- js/main.js
+++ js/main.js
@@ -7,3 +7,112 @@ owl.owlCarousel({
     autoplayTimeout:3000,
     mouseDrag: false
 });
+
+$(function () {
+	$(window).scroll(function() {
+	    $('.about-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__fadeInDown");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('.request-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__fadeInDown");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.methods__list').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__slideInLeft");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('.service-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__slideInLeft");
+	        }
+	    });
+	});
+
+    $(window).scroll(function() {
+	    $('.nutrition-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__slideInLeft");
+	        }
+	    });
+	});
+
+    $(window).scroll(function() {
+	    $('.programm-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__slideInLeft");
+	        }
+	    });
+	});
+
+    $(window).scroll(function() {
+	    $('.mega-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__slideInLeft");
+	        }
+	    });
+	});
+
+    $(window).scroll(function() {
+	    $('.protocol-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__slideInLeft");
+	        }
+	    });
+	});
+
+    $(window).scroll(function() {
+	    $('.check-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__slideInLeft");
+	        }
+	    });
+	});
+
+    $(window).scroll(function() {
+	    $('.form-block').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__animated animate__fadeInDown");
+	        }
+	    });
+	});
+})
